bc8dfe3867e2079dd267d80b37463f63f0bc772c
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-02-10 12:44:43 -0800

    Add: setup single product page

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,7 +18,10 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  featured_products:[]
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {}
 }
 
 const ProductsContext = React.createContext()
@@ -42,7 +45,17 @@ export const ProductsProvider = ({ children }) => {
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR });
     }
-    
+  }
+
+  const fetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
+    } catch (error) {
+      dispatch({type: GET_SINGLE_PRODUCT_ERROR})
+    }
     
   }
 
@@ -53,7 +66,7 @@ export const ProductsProvider = ({ children }) => {
 
 
   return (
-    <ProductsContext.Provider value={ {...state, openSidebar, closeSidebar } }>
+    <ProductsContext.Provider value={ {...state, openSidebar, closeSidebar, fetchSingleProduct } }>
       {children}
     </ProductsContext.Provider>
   )
